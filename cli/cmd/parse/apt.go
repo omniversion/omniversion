61592966532afcbcb75e51889ca9a1113e92d718
Roman Priebe <roman.priebe@layer9.berlin>
2022-05-02 19:44:45 +0200

    refactor: move cli into separate directory, clean up

--- cli/cmd/parse/apt.go
+++ cli/cmd/parse/apt.go
@@ -2,19 +2,19 @@ package parse
 
 import (
 	"github.com/hashicorp/go-multierror"
-	models2 "github.com/omniversion/omniversion/cli/models"
+	"github.com/omniversion/omniversion/cli/models"
 	"github.com/spf13/cobra"
 	"regexp"
 	"strings"
 )
 
-func parseAptOutput(input string) ([]models2.Dependency, error) {
+func parseAptOutput(input string) ([]models.Dependency, error) {
 	compiledRegex := regexp.MustCompile(`(?m)^(?P<name>.*?)/(?P<sources>\S*) (?P<version>\S*) (?P<architecture>\S*)( \[(?P<installed>installed)?(,(?P<automatic>automatic))?(,upgradable to: (?P<latest>.*))?(upgradable from: (?P<outdatedVersion>.*))?])?$`)
 	matches := compiledRegex.FindAllStringSubmatch(input, -1)
-	result := make([]models2.Dependency, 0, len(matches))
+	result := make([]models.Dependency, 0, len(matches))
 	var allErrors *multierror.Error
 	for _, match := range matches {
-		newDependency := models2.Dependency{
+		newDependency := models.Dependency{
 			Pm: "apt",
 		}
 		version := match[compiledRegex.SubexpIndex("version")]
@@ -25,7 +25,7 @@ func parseAptOutput(input string) ([]models2.Dependency, error) {
 			newDependency.Latest = latest
 			if isInstalled {
 				newDependency.Version = version
-				newDependency.Installed = []models2.InstalledDependency{{
+				newDependency.Installed = []models.InstalledDependency{{
 					Version: version,
 				}}
 			} else {
@@ -34,7 +34,7 @@ func parseAptOutput(input string) ([]models2.Dependency, error) {
 		} else {
 			// we are dealing with the output of an `outdated` command
 			newDependency.Version = outdatedVersion
-			newDependency.Installed = []models2.InstalledDependency{{
+			newDependency.Installed = []models.InstalledDependency{{
 				Version: outdatedVersion,
 			}}
 			newDependency.Latest = version
--- cli/go.mod
+++ cli/go.mod
@@ -1,20 +1,3 @@
-module github.com/omniversion/omniversion-cli
+module github.com/omniversion/omniversion
 
 go 1.17
-
-require (
-	github.com/hashicorp/go-multierror v1.1.1
-	github.com/sirupsen/logrus v1.8.1
-	github.com/spf13/cobra v1.4.0
-	github.com/stretchr/testify v1.7.1
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
-)
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
-)
