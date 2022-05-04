ff0f2591d238b76bcc41736c0caf9852c12bc611
Roman Priebe <roman.priebe@layer9.berlin>
2022-05-04 17:40:41 +0200

    fix(python): fix import paths

--- python/omniversion/package_info/__init__.py
+++ python/omniversion/package_info/__init__.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-"""Model classes for various types of dependency"""
-from .list.available_updates import AvailableUpdates
-from .list.package_infos_list import PackageInfosList
-from .list.versions_match import VersionsMatch
-from .list.vulnerabilities import Vulnerabilities
+"""Model classes for various types of package infos"""
+from .list import AvailableUpdates, PackageInfosList, VersionsMatch, Vulnerabilities
 from .advisory import Advisory
 from .package_info import PackageInfo
---
+++ python/omniversion/package_info/list/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""Model classes for lists of package info"""
+from .available_updates import AvailableUpdates
+from .package_infos_list import PackageInfosList
+from .versions_match import VersionsMatch
+from .vulnerabilities import Vulnerabilities
